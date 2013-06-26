4eb800be327fe4c19ae167d4829f5d1b7c780a3b
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-06-25 21:51:35 -0700

    short options for the most common commands

--- flask_runner.py
+++ flask_runner.py
@@ -6,17 +6,17 @@ class Runner():
 
     def parse_args(self, args = None):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--host', metavar = 'HOST', type = str,
+        parser.add_argument('-H', '--host', metavar = 'HOST', type = str,
             default = '127.0.0.1',
             help = 'hostname or IP address to listen on (default is "127.0.0.1")')
-        parser.add_argument('--port', metavar = 'PORT', type = int,
+        parser.add_argument('-P', '--port', metavar = 'PORT', type = int,
             default = 5000,
             help = 'port of the web server (default is 5000)')
-        parser.add_argument('--debug', dest = 'debug', action = 'store_true',
+        parser.add_argument('-d', '--debug', dest = 'debug', action = 'store_true',
             help = 'enable the debugger')
         parser.add_argument('--noeval', dest = 'use_evalex', action = 'store_false',
             help = 'disable the exception evaluation feature in the debugger')
-        parser.add_argument('--reload', dest = 'use_reloader', action = 'store_true',
+        parser.add_argument('-r', '--reload', dest = 'use_reloader', action = 'store_true',
             help = 'reload the Python process when any modules are changed')
         parser.add_argument('--extra', metavar = 'FILE', type = str, dest = 'extra_files', action = 'append', 
             help = 'additional file for the reloader to watch for changes')
